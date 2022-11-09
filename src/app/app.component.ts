06f277522d030d8191b82922160f846c56da090d
Abdulrahman Hamed <abdelrahmanh.habashi@gmail.com>
2022-11-09 17:28:49 +0200

    Implement vacations component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Balance } from './model/balance';
+import { VacationsService } from './service/vacations.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vacation';
+  vacationTypes: any;
+  employeeId: any;
+  balance: Balance = {};
+  constructor(private vacationsService: VacationsService) {
+
+  }
+  ngOnInit() {
+    console.log("onInit")
+    this.vacationsService.getVacationTypes().subscribe(res => {
+      console.log(res);
+      this.vacationTypes = res.types;
+    })
+  }
+
+  getBalances(empId: String) {
+    this.vacationsService.getEmployeeVacationsBalance(+empId).subscribe(res => {
+      this.balance = res;
+    })
+  }
+  reserve(vacationType:any,start: any, end: any) {
+    this.vacationsService.createEmployeeVacation(this.employeeId,vacationType,end,start).subscribe(res=>{
+      this.getBalances(this.employeeId);
+    })
+  }
 }
---
+++ src/app/model/models.ts
@@ -0,0 +1,3 @@
+export * from './balance';
+export * from './vacation';
+export * from './vacationTypes';
